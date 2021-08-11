80d0f6d724c00865b167f1018e07a3d13961c1c1
Mert <79603812+ufcovo@users.noreply.github.com>
2021-08-11 11:03:53 +0300

    login submit button

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Frontend';
+
+  flag: boolean = false;
+  signUpButton = document.getElementById('signUp');
+  signInButton = document.getElementById('signIn');
+  container = document.getElementById('cont');
+  
 }
+
