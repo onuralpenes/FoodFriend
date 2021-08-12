8d8012a86857fb1674fc35b160db4e77061a88e2
Mert <79603812+ufcovo@users.noreply.github.com>
2021-08-12 17:53:22 +0300

    login component created

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
+import { MatFormFieldModule} from '@angular/material/form-field';
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], 
 })
 
 export class AppComponent {
   title = 'Frontend';
 
-  flag: boolean = false;
   
 }
 
+export class FormFieldOverviewExample {}
