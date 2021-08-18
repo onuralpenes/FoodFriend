19eb46883c894eb0edf78e3dbd7ff9594deab026
Mert <79603812+ufcovo@users.noreply.github.com>
2021-08-18 09:14:56 +0300

    food

--- Frontend/src/app/modules/private/private.module.ts
+++ Frontend/src/app/modules/private/private.module.ts
@@ -3,7 +3,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BodyComponent } from 'src/app/main-page/body/body.component';
 import { FooterComponent } from 'src/app/main-page/footer/footer.component';
 import { HeaderComponent } from 'src/app/main-page/header/header.component';
-import { FoodComponent } from 'src/app/main-page/layout/food/food.component';
 import { MainComponent } from 'src/app/main-page/main/main.component';
 import { CustomMaterialModule } from '../material/material.module';
 import { PrivateLayoutComponent } from './private-layout.component';
@@ -16,7 +15,6 @@ import { PrivateRoutingModule } from './private-routing.module';
         FooterComponent,
         MainComponent,
         BodyComponent,
-        FoodComponent,
     ],
     imports: [CommonModule,PrivateRoutingModule, CustomMaterialModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
