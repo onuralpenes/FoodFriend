1ff3266d26aba1ff3c13ec24c0dda2579070250f
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-09-15 11:21:19 +0300

    Update health-info.model.ts

--- Frontend/src/app/models/user/user-info/health/health-info.model.ts
+++ Frontend/src/app/models/user/user-info/health/health-info.model.ts
@@ -1,9 +1,13 @@
+import { AllergyList } from "./allergy-detail.model";
+import { IllnessList } from "./illness-detail.model";
+import { PregnantList } from "./pregnant-detail.model";
+
 export interface HealthInfoDto {
     healthInfoId: number;
     hasHealthProblem: boolean;
     hasAllergy: boolean;
     isPregnant: boolean;
-    //illnessDetailList: IllnessList;
-    //allergyDetailList: AllergyList;
-    //pregnantDetailList: PregnantList;
+    illnessDetailList: IllnessList;
+    allergyDetailList: AllergyList;
+    pregnantDetailList: PregnantList;
 }
