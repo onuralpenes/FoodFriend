1b34ee785481c45fab9782fcc59807563aff577b
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-09-23 16:56:52 +0300

    Profile prepared

--- Frontend/src/app/main-page/layout/profile/profile.component.ts
+++ Frontend/src/app/main-page/layout/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { UserInfo } from 'src/app/models/user/user-info/user-info.model';
@@ -18,7 +18,7 @@ export class ProfileComponent implements OnInit {
   weight = 85;
   height = 181;
   age = 21;
-  birthdate = new Date(1999,6,2);
+  birthdate = new Date(1999, 6, 2);
   userName = "Onuralp Enes Öz";
   email = "oz.onuralp@gmail.com";
   phone = "555-555-5555";
@@ -27,60 +27,78 @@ export class ProfileComponent implements OnInit {
   genderless = false;
   male = false;
   female = false;
-  
+
 
   illnessInfo =
     {
       "hasIllness": true,
 
-      "illnesses": [
+      "illnessList": [
         {
-          "name": "Hastalık1",
-          "exp": "bos"
+          "illnessName": "Hastalık1",
+          "illnessType": 0
         },
         {
-          "name": "Hastalık2",
-          "exp": "bos"
+          "illnessName": "Hastalık2",
+          "illnessType": 0
         }
       ]
     }
 
-  allergy = {
-    "hasAllergy":true,
-    "allergies": [
+  allergyInfo = {
+    "hasAllergy": true,
+    "allergyList": [
       {
-        "name": "Alerji1",
-        "exp": "bos"
+        "allergyName": "Alerji1",
+        "allergyType": 0
       }, {
-        "name": "Alerji2",
-        "exp": "bos"
+        "allergyName": "Alerji2",
+        "allergyType": 0
       }, {
-        "name": "Alerji3",
-        "exp": "bos"
+        "allergyName": "Alerji3",
+        "allergyType": 0
       }, {
-        "name": "Alerji4",
-        "exp": "bos"
+        "allergyName": "Alerji4",
+        "allergyType": 0
       },
     ]
   }
 
-  constructor(public modal: MatDialog, entityService: HttpEntityRepositoryService<UserInfo>, authService: AuthService){
+  pregnantInfo = {
+    "isPregnant": false,
+    "pregnantList": []
+  }
+
+  disabledInfo = {
+    "isDisabled": false,
+    "disabledList": []
+  }
+
+  constructor(public modal: MatDialog, entityService: HttpEntityRepositoryService<UserInfo>, authService: AuthService) {
     this.user = entityService.get("/User/GetUserInfo?userId=", authService.CurrentUserId);
     // this.userName = this.user.firstName + " " + this.user.lastName
     // this.email = this.user.emailAddress
     // this.phone = this.user.phone
     // this.birthdate = this.user.birthDate
-    this.age = Math.floor(((Math.abs(Date.now() - this.birthdate.getTime())) / (1000 * 3600 * 24))/365.25);
+    this.age = Math.floor(((Math.abs(Date.now() - this.birthdate.getTime())) / (1000 * 3600 * 24)) / 365.25);
     // this.height = this.user.physicalInfo.height
     // this.weight = this.user.physicalInfo.weight
+    // this.illnessInfo.hasIllness = this.user.healthInfo.hasHealthProblem
+    // this.illnessInfo.illnessList = this.user.healthInfo.illnessDetailList
+    // this.allergyInfo.hasAllergy = this.user.healthInfo.hasAllergy
+    // this.allergyInfo.allergyList = this.user.healthInfo.allergyDetailList
+    // this.pregnantInfo.isPregnant = this.user.healthInfo.isPregnant
+    // this.pregnantInfo.pregnantList = this.user.healthInfo.pregnantDetailList
+    // this.disabledInfo.isDisabled = this.user.physicalInfo.disabledStatus
+    // this.disabledInfo.disabledList = this.user.physicalInfo.disabledInfo
   }
 
-  editProfile(){
+  editProfile() {
     this.modal.open(SurveyComponent);
   }
 
   ngOnInit(): void {
-    
+
     if (this.gender == "male") {
       this.male = true;
       this.female = false;
