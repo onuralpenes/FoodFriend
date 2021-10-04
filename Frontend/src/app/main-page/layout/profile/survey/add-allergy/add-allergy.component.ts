dafcb78d5cac49f62fc5d459cab136e9347995da
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-10-04 17:32:17 +0300

    add allergy,illness,disabled

--- Frontend/src/app/main-page/layout/profile/survey/add-allergy/add-allergy.component.ts
+++ Frontend/src/app/main-page/layout/profile/survey/add-allergy/add-allergy.component.ts
@@ -2,25 +2,50 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { AlertService } from "src/app/helpers/alert.service";
 import { AllergyDetail } from "src/app/models/user/health-info/allergy-detail.model";
+import { AuthService } from "src/app/services/auth.service";
 import { HttpEntityRepositoryService } from "src/app/services/http-entity-repository.service";
 
 @Component({
-    selector: 'app-add-allergy',
-    templateUrl: './add-allergy.component.html',
-    styleUrls: ['./add-allergy.component.css'],
+  selector: 'app-add-allergy',
+  templateUrl: './add-allergy.component.html',
+  styleUrls: ['./add-allergy.component.css'],
 })
-export class AddAllergy{
-    allergyForm!: FormGroup;
-    constructor(private formBuilder: FormBuilder, private entityService: HttpEntityRepositoryService<AllergyDetail>, private alertService: AlertService) { 
-        this.allergyForm = this.formBuilder.group({
-            allergyName: new FormControl('', [Validators.required])
-          });
+export class AddAllergy {
+  allergyForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private alertService: AlertService,
+    private entityService: HttpEntityRepositoryService<AllergyDetail>, private authService: AuthService) {
+    this.allergyForm = this.formBuilder.group({
+      allergyName: new FormControl('', [Validators.required])
+    });
+  }
+
+  addAllergy() {
+    if (this.allergyForm.invalid) {
+      this.alertService.openSnackBar(false, " ");
+      return;
     }
 
-    addAllergy() { 
-      if (this.allergyForm.invalid) {
-        this.alertService.openSnackBar(false," ");
+    this.entityService.get("/User/Get?userId=", this.authService.CurrentUserId).subscribe(data => {
+      var Data: any = data;
+      if (!Data.success) {
+        this.alertService.openSnackBar(Data.success, Data.message);
         return;
       }
-    }
+      const allPost: AllergyDetail = {
+        allergyDetailId: 0,
+        healthInfoId: Data.data.healthInfoId,
+        allergyName: this.allergyForm.value.allergyName
+      }
+      this.entityService.insert("/AllergyDetail/Add", allPost).subscribe(data => {
+        var post: any = data;
+        console.log(post);
+        if (!post.success) {
+          this.alertService.openSnackBar(post.success, post.message);
+          return;
+        }
+
+      });
+    });
+    this.alertService.openSnackBar(true, "Added");
+  }
 }
--- Frontend/src/app/main-page/layout/profile/survey/add-illness/add-illness.component.ts
+++ Frontend/src/app/main-page/layout/profile/survey/add-illness/add-illness.component.ts
@@ -2,25 +2,51 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { AlertService } from "src/app/helpers/alert.service";
 import { IllnessDetail } from "src/app/models/user/health-info/illness-detail.model";
+import { AuthService } from "src/app/services/auth.service";
 import { HttpEntityRepositoryService } from "src/app/services/http-entity-repository.service";
 
 @Component({
-    selector: 'app-add-illness',
-    templateUrl: './add-illness.component.html',
-    styleUrls: ['./add-illness.component.css'],
+  selector: 'app-add-illness',
+  templateUrl: './add-illness.component.html',
+  styleUrls: ['./add-illness.component.css'],
 })
 export class AddIllness {
-    illnessForm!: FormGroup;
-    constructor(private formBuilder: FormBuilder, private entityService: HttpEntityRepositoryService<IllnessDetail>, private alertService: AlertService) { 
-        this.illnessForm = this.formBuilder.group({
-            illnessName: new FormControl('', [Validators.required])
-          });
+  illnessForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private alertService: AlertService,
+    private entityService: HttpEntityRepositoryService<IllnessDetail>, private authService: AuthService) {
+    this.illnessForm = this.formBuilder.group({
+      illnessName: new FormControl('', [Validators.required])
+    });
+  }
+
+  addIllness() {
+    if (this.illnessForm.invalid) {
+      this.alertService.openSnackBar(false, " ");
+      return;
     }
 
-    addIllness() { 
-      if (this.illnessForm.invalid) {
-        this.alertService.openSnackBar(false," ");
+    this.entityService.get("/User/Get?userId=", this.authService.CurrentUserId).subscribe(data => {
+      var Data: any = data;
+      if (!Data.success) {
+        this.alertService.openSnackBar(Data.success, Data.message);
         return;
       }
-    }
+      const illPost: IllnessDetail = {
+        illnessDetailId: 0,
+        healthInfoId: Data.data.healthInfoId,
+        illnessName: this.illnessForm.value.illnessName
+      }
+      this.entityService.insert("/IllnessDetail/Add", illPost).subscribe(data => {
+        var post: any = data;
+        console.log(post);
+        if (!post.success) {
+          this.alertService.openSnackBar(post.success, post.message);
+          return;
+        }
+
+      });
+    });
+    this.alertService.openSnackBar(true, "Added");
+  }
+
 }
