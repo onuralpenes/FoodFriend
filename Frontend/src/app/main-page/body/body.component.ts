21846fe7d6b7df33b8b8b18075d796bd62ffbc5c
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-10-04 14:48:00 +0300

    Body Update

--- Frontend/src/app/main-page/body/body.component.ts
+++ Frontend/src/app/main-page/body/body.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { AlertService } from 'src/app/helpers/alert.service';
+import { User } from 'src/app/models/user/user.model';
 import { PrivateLayoutComponent } from 'src/app/modules/private/private-layout.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpEntityRepositoryService } from 'src/app/services/http-entity-repository.service';
 
 @Component({
   selector: 'app-body',
@@ -12,7 +16,22 @@ export class BodyComponent {
   @Output() pName: EventEmitter<any> = new EventEmitter();
   @Input() opened;
   @Input() pinButton = false;
-  constructor(private priv: PrivateLayoutComponent) { }
+
+  name!: string;
+  surname !: string;
+  constructor(private priv: PrivateLayoutComponent, authService: AuthService, private alertService: AlertService, entityService: HttpEntityRepositoryService<User>) { 
+    entityService.get("/User/Get?userId=", authService.CurrentUserId).subscribe(data => {
+      var Data: any = data;
+      if (!Data.success) {
+        this.alertService.openSnackBar(Data.success, Data.message);
+        return;
+      }
+
+      this.name = Data.data.firstName;
+      this.surname = Data.data.lastName;
+    });
+  }
+  
   pinSidebar() {
     this.pinButton = !this.pinButton;
     this.pin.emit(this.pinButton);
@@ -21,4 +40,5 @@ export class BodyComponent {
   changeLang(langCode: string) {
     this.priv.changeLang(langCode);
   }
+
 }
