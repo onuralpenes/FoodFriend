a9944ab3c742e4b7f6c178457f51105f0e83f536
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-10-11 17:58:21 +0300

    not-found page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:'',
@@ -8,7 +9,9 @@ const routes: Routes = [
   
 
   {path: 'login' , component: LoginComponent},
-  {path: '', redirectTo : "login", pathMatch: 'full'}
+  {path: '', redirectTo : "login", pathMatch: 'full'},
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
