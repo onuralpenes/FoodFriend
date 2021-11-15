f748b98a703bc73603721b47ed92b0821da530d2
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-15 13:58:04 +0300

    Update Profile

--- Frontend/src/app/main-page/layout/profile/survey/survey.component.ts
+++ Frontend/src/app/main-page/layout/profile/survey/survey.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { AlertService } from 'src/app/helpers/alert.service';
+import { User } from 'src/app/models/user/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpEntityRepositoryService } from 'src/app/services/http-entity-repository.service';
 import { AddAllergy } from './add-allergy/add-allergy.component';
 import { AddDisability } from './add-disable/add-disability.component';
 import { AddIllness } from './add-illness/add-illness.component';
@@ -16,9 +20,9 @@ export class SurveyComponent implements OnInit {
   surveyForm!: FormGroup;
   post: any = '';
 
-  constructor(private formBuilder: FormBuilder, private modal: MatDialog) { }
-  
-  ngOnInit() { 
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private modal: MatDialog, private entityService: HttpEntityRepositoryService<User>, private alertService: AlertService) { }
+
+  ngOnInit() {
     this.surveyForm = this.formBuilder.group({
       'firstName': new FormControl('', [Validators.required]),
       'lastName': new FormControl('', [Validators.required]),
@@ -29,20 +33,34 @@ export class SurveyComponent implements OnInit {
       'birthDate': new FormControl('', [Validators.required]),
     });
   }
-  
-  onSubmit(post) {
-    this.post = post;
+
+  onSubmit() {
+    let updateData: any = {
+      userId: this.authService.CurrentUserId,
+      emailAddress: this.surveyForm.value.emailAddress,
+      birthDate: this.surveyForm.value.birthDate,
+      firstName: this.surveyForm.value.firstName,
+      lastName: this.surveyForm.value.lastName,
+      phone: this.surveyForm.value.phoneNumber
+    }
+    this.entityService.update("/User/Update", updateData).subscribe(data => {
+      var Data: any = data;
+      if (!Data.success) {
+        this.alertService.openSnackBar(Data.success, Data.message);
+        return;
+      }
+    });
   }
 
-  addIllness(){
+  addIllness() {
     this.modal.open(AddIllness);
   }
 
-  addAllergy(){
+  addAllergy() {
     this.modal.open(AddAllergy);
   }
 
-  addDisability(){
+  addDisability() {
     this.modal.open(AddDisability);
   }
 }
