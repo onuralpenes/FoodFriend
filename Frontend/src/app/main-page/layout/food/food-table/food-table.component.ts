1ab777c259f10402fb4ace11efa9eac8ef61746d
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-16 11:39:03 +0300

    eatact pagin deleted

--- Frontend/src/app/main-page/layout/food/food-table/food-table.component.ts
+++ Frontend/src/app/main-page/layout/food/food-table/food-table.component.ts
@@ -100,7 +100,6 @@ export class FoodTableComponent implements AfterViewInit {
   dataSource = new MatTableDataSource(this.eatTable);
 
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   delete(id: number, name: string) {
     this.modal.open(ConfirmModalComponent, {
@@ -142,55 +141,5 @@ export class FoodTableComponent implements AfterViewInit {
     }
   }
 
-  ngAfterViewInit() {
-    this.changeLang();
-    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      this.changeLang();
-    });
-  }
-
-  changeLang() {
-    this.translate.get('paginator.item').subscribe((data: any) => {
-      this.paginator._intl.itemsPerPageLabel = data;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-    this.translate.get('paginator.next').subscribe((data: any) => {
-      this.paginator._intl.nextPageLabel = data;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-    this.translate.get('paginator.previous').subscribe((data: any) => {
-      this.paginator._intl.previousPageLabel = data;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-    this.translate.get('paginator.last').subscribe((data: any) => {
-      this.paginator._intl.lastPageLabel = data;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-    this.translate.get('paginator.first').subscribe((data: any) => {
-      this.paginator._intl.firstPageLabel = data;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-    this.translate.get('paginator.range').subscribe((data: any) => {
-      var range = (page: number, pageSize: number, length: number) => {
-        if (length == 0 || pageSize == 0) { return `0 ${data} ${length}`; }
-
-        length = Math.max(length, 0);
-
-        const startIndex = page * pageSize;
-        const endIndex = startIndex < length ?
-          Math.min(startIndex + pageSize, length) :
-          startIndex + pageSize;
-        if (data == "de") { return `${length} ${data}  ${startIndex + 1} - ${endIndex}`; }
-        else { return `${startIndex + 1} - ${endIndex} ${data} ${length}`; }
-      };
-      this.paginator._intl.getRangeLabel = range;
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    });
-  }
+  ngAfterViewInit() {}
 }
