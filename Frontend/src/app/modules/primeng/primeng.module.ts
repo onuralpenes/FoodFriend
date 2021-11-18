12c068172feb261a4001e3a68a5fb0b33eddbff8
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-18 08:56:02 +0300

    Tables

--- Frontend/src/app/modules/primeng/primeng.module.ts
+++ Frontend/src/app/modules/primeng/primeng.module.ts
@@ -3,19 +3,22 @@ import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { PaginatorModule } from 'primeng/paginator';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
     imports: [
         DialogModule,
         InputTextModule,
         TableModule,
-        PaginatorModule
+        PaginatorModule,
+        ButtonModule,
     ],
     exports: [
         DialogModule,
         InputTextModule,
         TableModule,
-        PaginatorModule
+        PaginatorModule,
+        ButtonModule,
     ]
 
 })
