c8c0f1d710f5c2345079c488be77dd4c504a7b0e
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-24 11:37:38 +0300

    counselee profile & patient trace code update
    Both not completed due to validation issues

--- Frontend/src/app/main-page/layout/counselee-profile/counselee-profile.component.ts
+++ Frontend/src/app/main-page/layout/counselee-profile/counselee-profile.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertService } from 'src/app/helpers/alert.service';
-import { HealthInfo } from 'src/app/models/user/health-info/health-info.model';
-import { PhysicalInfo } from 'src/app/models/user/physical-info/pysical-info.model';
 import { User } from 'src/app/models/user/user.model';
 import { HttpEntityRepositoryService } from 'src/app/services/http-entity-repository.service';
-import { PatientTraceTableComponent } from '../patient-trace/patient-trace-table/patient-trace-table.component';
 import { AllergyList, DisabledList, IllnessList, PregnantList } from '../profile/profile.component';
 
 @Component({
@@ -27,7 +24,6 @@ export class CounseleeProfileComponent implements OnInit {
   hasAllergy!: boolean;
   isPregnant!: boolean;
   isDisabled!: boolean;
-  dummy = "dummy";
   gender = "male";
   genderless = false;
   male = false;
@@ -40,61 +36,57 @@ export class CounseleeProfileComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private alertService: AlertService, entityService: HttpEntityRepositoryService<User>) {
 
-   this.route.paramMap.subscribe(param => {
-    let id = Number(param.get('id'));
+    this.route.paramMap.subscribe(param => {
+      let id = Number(param.get('id'));
 
-    entityService.get("/User/Get?userId=", id).subscribe(data => {
+      entityService.get("/User/Get?userId=", id).subscribe(data => {
 
-      var Data: any = data;
-      if (!Data.success) {
-        this.alertService.openSnackBar(Data.success, Data.message);
-        return;
-      }
-
-      this.userName = Data.data.firstName + " " + Data.data.lastName;
-      this.email = Data.data.emailAddress;
-      this.phone = Data.data.phone;
-      this.birthdate = Data.data.birthDate;
-      this.healthId = Data.data.healthInfoId;
-      this.physicalId = Data.data.physicalInfoId;
-
-      const convertAge = new Date(this.birthdate);
-      const timeDiff = Math.abs(Date.now() - convertAge.getTime());
-      this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
-
-      entityService.get("/PhysicalInfo/Get?id=", this.physicalId).subscribe(data => {
-        var DataPh: any = data;
-        if (!DataPh.success) {
-          this.alertService.openSnackBar(DataPh.success, DataPh.message);
-          return;
-        }
-        this.height = DataPh.data.height;
-        this.weight = DataPh.data.weight;
-        this.isDisabled = DataPh.data.disabledStatus;
-        this.disabledList = DataPh.data.disabledDetails;
-      });
-
-      entityService.get("/HealthInfo/Get?id=", this.healthId).subscribe(data => {
-
-        var DataH: any = data;
-        if (!DataH.success) {
-          this.alertService.openSnackBar(DataH.success, DataH.message);
+        var Data: any = data;
+        if (!Data.success) {
+          this.alertService.openSnackBar(Data.success, Data.message);
           return;
         }
 
-        this.hasIllness = DataH.data.hasHealthProblem;
-        this.hasAllergy = DataH.data.hasAllergy;
-        this.isPregnant = DataH.data.isPregnant;
-        this.illnessList = DataH.data.illnessDetails;
-        this.allergyList = DataH.data.allergyDetails;
-        this.pregnantList = DataH.data.pregnantDetails;
+        this.userName = Data.data.firstName + " " + Data.data.lastName;
+        this.email = Data.data.emailAddress;
+        this.phone = Data.data.phone;
+        this.birthdate = Data.data.birthDate;
+        this.healthId = Data.data.healthInfoId;
+        this.physicalId = Data.data.physicalInfoId;
+
+        const convertAge = new Date(this.birthdate);
+        const timeDiff = Math.abs(Date.now() - convertAge.getTime());
+        this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
+
+        entityService.get("/PhysicalInfo/Get?id=", this.physicalId).subscribe(data => {
+          var DataPh: any = data;
+          if (!DataPh.success) {
+            this.alertService.openSnackBar(DataPh.success, DataPh.message);
+            return;
+          }
+          this.height = DataPh.data.height;
+          this.weight = DataPh.data.weight;
+          this.isDisabled = DataPh.data.disabledStatus;
+          this.disabledList = DataPh.data.disabledDetails;
+        });
+
+        entityService.get("/HealthInfo/Get?id=", this.healthId).subscribe(data => {
+
+          var DataH: any = data;
+          if (!DataH.success) {
+            this.alertService.openSnackBar(DataH.success, DataH.message);
+            return;
+          }
+
+          this.hasIllness = DataH.data.hasHealthProblem;
+          this.hasAllergy = DataH.data.hasAllergy;
+          this.isPregnant = DataH.data.isPregnant;
+          this.illnessList = DataH.data.illnessDetails;
+          this.allergyList = DataH.data.allergyDetails;
+          this.pregnantList = DataH.data.pregnantDetails;
+        });
       });
-    });
-
-})
-
-    
-
+    })
   }
 
   ngOnInit(): void {
--- Frontend/src/app/main-page/layout/patient-trace/patient-trace-table/target-card/target-card.component.ts
+++ Frontend/src/app/main-page/layout/patient-trace/patient-trace-table/target-card/target-card.component.ts
@@ -5,65 +5,64 @@ import { ProgressBarMode } from "@angular/material/progress-bar";
 import { Transfer2 } from "../patient-trace-table.component";
 
 @Component({
-    selector: 'app-target-card',
-    templateUrl: './target-card.html',
-    styleUrls: ['./target-card.css'],
-  })
-  export class PatientTargetCard {
-  
-    constructor(@Inject(MAT_DIALOG_DATA) public data: Transfer2) {
-      if (!data.target) {
-        this.target = "Lose Weight";
-      }
-      else {
-        this.target = "Gain Weight";
-      }
-      if (data.startingDate == null) {
-        this.startingDate = new Date();
-      }
-      else {
-        this.startingDate = data.startingDate;
-      }
-      if (data.endDate == null) {
-        this.endDate = "unspecified";
-      }
-      else {
-        var month = data.endDate.getUTCMonth() + 1;
-        var day = data.endDate.getUTCDate();
-        var year = data.endDate.getUTCFullYear();
-        this.endDate = day + "/" + month + "/" + year;
-      }
-      if (data.startingWeight == null) {
-        this.startingWeight = 100;
-      }
-      else {
-        this.startingWeight = data.startingWeight;
-      }
-      this.targetWeight = this.data.targetWeight;
-      this.currentWeight = this.data.currentWeight;
-      this.weight_percent = 100 * (this.startingWeight - this.currentWeight) / (this.startingWeight - this.targetWeight);
-      if (data.endDate == null) {
-        this.dateProg = 0;
-      }
-      else {
-        var currentProgress = this.currentDate.getTime() - this.startingDate.getTime();
-        var endProgress = data.endDate.getTime() - this.startingDate.getTime();
-        this.dateProg = 100 * currentProgress / endProgress;
-      }
+  selector: 'app-target-card',
+  templateUrl: './target-card.html',
+  styleUrls: ['./target-card.css'],
+})
+export class PatientTargetCard {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Transfer2) {
+    if (!data.target) {
+      this.target = "Lose Weight";
     }
-  
-    target!: string;
-    startingDate !: Date;
-    startingWeight !: number;
-    endDate: any;
-    targetWeight !: number;
-    currentDate: Date = new Date();
-    currentWeight !: number;
-    weight_percent !: number;
-    dateProg !: number;
-  
-    color: ThemePalette = 'warn';
-    mode: ProgressBarMode = 'determinate';
-  
+    else {
+      this.target = "Gain Weight";
+    }
+    if (data.startingDate == null) {
+      this.startingDate = new Date();
+    }
+    else {
+      this.startingDate = data.startingDate;
+    }
+    if (data.endDate == null) {
+      this.endDate = "unspecified";
+    }
+    else {
+      var month = data.endDate.getUTCMonth() + 1;
+      var day = data.endDate.getUTCDate();
+      var year = data.endDate.getUTCFullYear();
+      this.endDate = day + "/" + month + "/" + year;
+    }
+    if (data.startingWeight == null) {
+      this.startingWeight = 100;
+    }
+    else {
+      this.startingWeight = data.startingWeight;
+    }
+    this.targetWeight = this.data.targetWeight;
+    this.currentWeight = this.data.currentWeight;
+    this.weight_percent = 100 * (this.startingWeight - this.currentWeight) / (this.startingWeight - this.targetWeight);
+    if (data.endDate == null) {
+      this.dateProg = 0;
+    }
+    else {
+      var currentProgress = this.currentDate.getTime() - this.startingDate.getTime();
+      var endProgress = data.endDate.getTime() - this.startingDate.getTime();
+      this.dateProg = 100 * currentProgress / endProgress;
+    }
   }
-  
+
+  target!: string;
+  startingDate !: Date;
+  startingWeight !: number;
+  endDate: any;
+  targetWeight !: number;
+  currentDate: Date = new Date();
+  currentWeight !: number;
+  weight_percent !: number;
+  dateProg !: number;
+
+  color: ThemePalette = 'warn';
+  mode: ProgressBarMode = 'determinate';
+
+}
