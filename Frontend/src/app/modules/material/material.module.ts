8b23e4c171a24a076b64f1275396ac8bd5172b6e
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-24 16:23:06 +0300

    Update material.module.ts

--- Frontend/src/app/modules/material/material.module.ts
+++ Frontend/src/app/modules/material/material.module.ts
@@ -9,7 +9,6 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -29,7 +28,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSidenavModule,
     MatNativeDateModule,
     MatExpansionModule,
-    MatSortModule,
     MatTableModule,
     MatProgressBarModule,
     MatDialogModule,
@@ -48,7 +46,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSidenavModule,
     MatNativeDateModule,
     MatExpansionModule,
-    MatSortModule,
     MatTableModule,
     MatProgressBarModule,
     MatDialogModule,
