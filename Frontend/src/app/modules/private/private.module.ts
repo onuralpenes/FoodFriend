744e9782cc8ed60306473f3babb3d193c621cfa0
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-24 13:10:21 +0300

    Activity Update

--- Frontend/src/app/modules/private/private.module.ts
+++ Frontend/src/app/modules/private/private.module.ts
@@ -8,7 +8,6 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BodyComponent } from 'src/app/main-page/body/body.component';
 import { FooterComponent } from 'src/app/main-page/footer/footer.component';
 import { HeaderComponent } from 'src/app/main-page/header/header.component';
-import { ActivityTableComponent } from 'src/app/main-page/layout/activity/activity-table/activity-table.component';
 import { ActivityComponent } from 'src/app/main-page/layout/activity/activity.component';
 import { CounseleeProfileComponent } from 'src/app/main-page/layout/counselee-profile/counselee-profile.component';
 import { ConsumedCalorieComponent } from 'src/app/main-page/layout/dashboard/consumed-calorie/consumed-calorie.component';
@@ -21,7 +20,6 @@ import { FoodListComponent } from 'src/app/main-page/layout/food-list/food-list.
 import { AddFood } from 'src/app/main-page/layout/food/food-table/add-food/add-food.component';
 import { CustomFoodComponent } from 'src/app/main-page/layout/food/food-table/add-food/custom-food/custom-food.component';
 import { EditFood } from 'src/app/main-page/layout/food/food-table/edit-food/edit-table.component';
-import { FoodTableComponent } from 'src/app/main-page/layout/food/food-table/food-table.component';
 import { FoodComponent } from 'src/app/main-page/layout/food/food.component';
 import { ActivityTable } from 'src/app/main-page/layout/patient-trace/patient-trace-table/activity-table/activity-table.component';
 import { NutritionTable } from 'src/app/main-page/layout/patient-trace/patient-trace-table/nutrition-table/nutrition-table.component';
@@ -43,6 +41,7 @@ import { PrimeNgModule } from '../primeng/primeng.module';
 import { ActivityFilterPipe } from 'src/app/helpers/activity-filter.pipe';
 import { EditService } from 'src/app/helpers/edit.service';
 import { CustomFoodService } from 'src/app/helpers/custom-food.pipe';
+import { FoodTableComponent } from 'src/app/main-page/layout/food/food-table/food-table.component';
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
@@ -61,7 +60,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     FoodComponent,
     FoodTableComponent,
     ActivityComponent,
-    ActivityTableComponent,
     HealthCardComponent,
     SurveyComponent,
     PatientTraceComponent,
