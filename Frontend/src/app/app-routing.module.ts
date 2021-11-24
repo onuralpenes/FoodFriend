1df884277450fcdace4d07afe1b80027f53636a3
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-11-24 10:29:26 +0300

    app update

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  {path:'',
-  loadChildren: ()=> import('./modules/private/private.module').then(m=> m.PrivateModule)},
-  
+  {
+    path: '',
+    loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule)
+  },
 
-  {path: 'login' , component: LoginComponent},
-  {path: '', redirectTo : "login", pathMatch: 'full'},
-  {path: '404', component: NotFoundComponent},
-  {path: '**', redirectTo: '/404'}
+
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: "login", pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
