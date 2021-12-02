bd7647cf39e09cfdae9c6ce1c7058195ca9d1cd9
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-12-02 09:55:56 +0300

    update

--- Frontend/src/app/modules/material/material.module.ts
+++ Frontend/src/app/modules/material/material.module.ts
@@ -10,7 +10,6 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-
 @NgModule({
   imports: [
     MatFormFieldModule,
