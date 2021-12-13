d32572e5bf9c9de74a9422f1a81c3d839291e379
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-12-13 16:02:41 +0300

    mat-icon, mat-form-field & mat-input removed

--- Frontend/src/app/modules/material/material.module.ts
+++ Frontend/src/app/modules/material/material.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatSnackBarModule,
   ],
   exports: [
-    MatFormFieldModule,
-    MatInputModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatSnackBarModule,
   ],
