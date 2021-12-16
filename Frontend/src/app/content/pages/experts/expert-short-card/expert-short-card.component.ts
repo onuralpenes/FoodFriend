231b952fe502143d95f057b59b5d38cedf6c1de7
valiantsword <56646087+valiantsword@users.noreply.github.com>
2021-12-16 13:03:41 +0300

    expert service connection and goal model added

--- Frontend/src/app/content/pages/experts/expert-short-card/expert-short-card.component.ts
+++ Frontend/src/app/content/pages/experts/expert-short-card/expert-short-card.component.ts
@@ -1,6 +1,9 @@
 import { Input } from '@angular/core';
 import { Component } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { User } from 'src/app/models/user/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpEntityRepositoryService } from 'src/app/services/http-entity-repository.service';
 
 @Component({
   selector: 'app-expert-short-card',
@@ -11,7 +14,7 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 export class ExpertShortCardComponent {
   @Input() expert;
 
-  constructor(private confirmationService: ConfirmationService, private messageService: MessageService) { }
+  constructor(private confirmationService: ConfirmationService, private messageService: MessageService, private authService: AuthService, private entityService: HttpEntityRepositoryService<User>) { }
 
   details = false;
 
@@ -25,7 +28,12 @@ export class ExpertShortCardComponent {
       header: 'Confirmation',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'The expert has been successfully authorized.' });
+        this.entityService.insert("/User/AssignmentProfessionnel?professionnelId=" + this.expert.userId + "&patientId=" + this.authService.CurrentUserId, {})
+          .subscribe(data => {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'The expert has been successfully authorized.' });
+          }, err => {
+            this.messageService.add({ severity: 'warn', summary: 'Unsuccess', detail: 'An error occurred while authorizing expert.' });
+          });
       },
       reject: () => {
         this.messageService.add({ severity: 'warn', summary: 'Unsuccess', detail: 'Expert not authorized.' });
